c6c10b3ce9e481945152589bfe85e1cac67e998b
AAswordman <1002153674@qq.com>
2025-12-12 13:25:04 +0800

    1.6.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.ai.assistance.operit"
         minSdk = 26
         targetSdk = 34
-        versionCode = 30
-        versionName = "1.6.3"
+        versionCode = 31
+        versionName = "1.6.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
