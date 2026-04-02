9b40f62fa2432d5e262fb88659ddbf6083cf812a
wufannet <wufannet@users.noreply.github.com>
2026-04-02 18:35:21 +0800

    fix 高德第一步本应点击打车但是触发了终点拦截器,必须大于第 1 步,解决第1步操作点击首页打车按钮,导致错误触发终点拦截器,直接点击底部的打车按钮吧.

--- app/src/main/java/com/ai/assistance/operit/ui/features/toolbox/screens/autoglmride/action_intercepter/RideGdInterceptor.kt
+++ app/src/main/java/com/ai/assistance/operit/ui/features/toolbox/screens/autoglmride/action_intercepter/RideGdInterceptor.kt
@@ -33,11 +33,12 @@ class RideGdInterceptor(
                 val destNewX = 272
                 val destNewY = 626
                 val yRange = 25
-
+                //x范围 201到 549  Y范围 601到651
                 if (absX in 201..549 && absY in (destNewY - yRange)..(destNewY + yRange) &&
                     (thinkingText.contains("搜索框") || thinkingText.contains("输入目的地") || thinkingText.contains(
                         "你要去哪儿") || thinkingText.contains("输入框")) &&
-                    (start.isEmpty() || stepCount > 2) //高德差异,要多一步,这个应该改为 3, 抽成配置和对象是最好的. 关键数据与逻辑.
+                    (start.isEmpty() || stepCount > 3) //高德差异,要多一步,这个应该改为 3, 抽成配置和对象是最好的. 关键数据与逻辑.
+                    && ( stepCount > 1) //必须大于第 1 步,解决第1步操作点击首页打车按钮,导致错误触发终点拦截器,直接点击底部的打车按钮吧.
                 ) {
                     val modifiedFields = action.fields.toMutableMap()
                     modifiedFields["element"] = "[$destNewX, $absY]"//只看不x坐标,解决点到搜索框右边推荐的地址问题
@@ -67,7 +68,7 @@ class RideGdInterceptor(
                 // 2. 点击起点坐标修改, 起点拦截器
                 val startNewX = 499
                 val startNewY = 560
-
+                //x范围 201到 550  Y范围 401到585
                 if (start.isNotEmpty() && (
                             (absX in 201..(startNewX + 49) && absY in 401..(startNewY + yRange - 1) && (thinkingText.contains(
                                 "起点"
